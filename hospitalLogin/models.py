6fb0d1c7983d5e9c3b7501801af2fe7e9d015ad8
Sharan Phadke <phadkesharan@gmail.com>
2022-01-29 20:40:37 +0530

    db and super user setup

--- hospitalLogin/models.py
+++ hospitalLogin/models.py
@@ -1,3 +1,19 @@
+from distutils.command.upload import upload
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+
+    firstName = models.CharField(max_length=50)
+    lastName = models.CharField(max_length=50)
+    profilePicture = models.ImageField(upload_to = 'uploads/')
+    username = models.CharField(max_length=50, primary_key=True)
+    emailId = models.EmailField()
+    password = models.CharField(max_length=20)
+    confirmPassword = models.CharField(max_length=20)
+    address = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.username
